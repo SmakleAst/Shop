f8fc03afc775a6a45c7867882061d7eac19d64a8
SmakleAst <SmakleLover@yandex.ru>
2024-05-10 23:00:03 +0300

    ShopDbContext implementation

---
+++ Shop.Backend/Shop.Persistence/ShopDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Domain;
+
+namespace Shop.Persistence
+{
+    public class ShopDbContext : DbContext, IShopDbContext
+    {
+        public DbSet<Product>  Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ShopConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
