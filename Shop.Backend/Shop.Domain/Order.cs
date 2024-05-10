d0b4ec56da029f14a858f56befaaf91a97fcd7c5
SmakleAst <SmakleLover@yandex.ru>
2024-05-10 22:54:29 +0300

    Add entities

---
+++ Shop.Backend/Shop.Domain/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Shop.Domain
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string  CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public Statuses Status { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set;}
+
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ Shop.Backend/Shop.Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop.Domain
+{
+    public class Product
+    {
+        public long ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set;}
+    }
+}
