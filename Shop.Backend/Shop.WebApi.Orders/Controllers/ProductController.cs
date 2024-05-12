f53cbcd4c4e1373dad15d5715bf94fdd0eb6de78
SmakleAst <SmakleLover@yandex.ru>
2024-05-12 19:07:06 +0300

    Base and product controllers implementation

---
+++ Shop.Backend/Shop.WebApi.Orders/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Products.Commands.CreateProduct;
+using Shop.Application.Products.Commands.DeleteProduct;
+using Shop.Application.Products.Commands.UpdateProduct;
+using Shop.Application.Products.Queries.GetProductDetails;
+using Shop.Application.Products.Queries.GetProductList;
+
+namespace Shop.WebApi.Orders.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : BaseController
+    {
+        private readonly IMapper _mapper;
+
+        public ProductController(IMapper mapper) =>
+            _mapper = mapper;
+
+        [HttpGet]
+        public async Task<ActionResult<ProductListVm>> GetAll()
+        {
+            var query = new GetProductListQuery
+            {
+
+            };
+
+            var vm = await Mediator.Send(query);
+
+            return Ok(vm);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDetailsVm>> Get(int id)
+        {
+            var query = new GetProductDetailsQuery
+            {
+                Id = id
+            };
+
+            var vm = await Mediator.Send(query);
+
+            return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromBody] CreateProductDto createProductDto)
+        {
+            var command = _mapper.Map<CreateProductCommand>(createProductDto);
+
+            var productId = await Mediator.Send(command);
+
+            return Ok(productId);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdateProductDto updateProductDto)
+        {
+            var command = _mapper.Map<UpdateProductCommand>(updateProductDto);
+
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var command = new DeleteProductCommand
+            {
+                Id = id
+            };
+
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
