5e99df07867044aaeb2f78f051557b47eabc1678
SmakleAst <SmakleLover@yandex.ru>
2024-05-12 19:15:07 +0300

    Add create and update product dto models

---
+++ Shop.Backend/Shop.WebApi.Orders/Models/CreateProductDto.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Shop.Application.Common.Mappings;
+using Shop.Application.Products.Commands.CreateProduct;
+
+namespace Shop.WebApi.Orders.Models
+{
+    public class CreateProductDto  : IMapWith<CreateProductCommand>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateProductDto, CreateProductCommand>()
+                .ForMember(productCommand => productCommand.Name,
+                    opt => opt.MapFrom(productDto => productDto.Name))
+                .ForMember(productCommand => productCommand.Description,
+                    opt => opt.MapFrom(productDto => productDto.Description))
+                .ForMember(productCommand => productCommand.Price,
+                    opt => opt.MapFrom(productDto => productDto.Price));
+        }
+    }
+}
---
+++ Shop.Backend/Shop.WebApi.Orders/Models/UpdateProductDto.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Shop.Application.Common.Mappings;
+using Shop.Application.Products.Commands.UpdateProduct;
+
+namespace Shop.WebApi.Orders.Models
+{
+    public class UpdateProductDto : IMapWith<UpdateProductCommand>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateProductDto, UpdateProductCommand>()
+                .ForMember(productCommand => productCommand.Id,
+                    opt => opt.MapFrom(productDto => productDto.Id))
+                .ForMember(productCommand => productCommand.Name,
+                    opt => opt.MapFrom(productDto => productDto.Name))
+                .ForMember(productCommand => productCommand.Description,
+                    opt => opt.MapFrom(productDto => productDto.Description))
+                .ForMember(productCommand => productCommand.Price,
+                    opt => opt.MapFrom(productDto => productDto.Price));
+        }
+    }
+}
