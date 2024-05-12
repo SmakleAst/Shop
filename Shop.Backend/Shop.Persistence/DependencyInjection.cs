feb6f14e144c36df2facfc922b24f3bef11065dd
SmakleAst <SmakleLover@yandex.ru>
2024-05-12 18:28:25 +0300

    Dependency injection configuration

---
+++ Shop.Backend/Shop.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Application.Interfaces;
+
+namespace Shop.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MSSQL");
+            services.AddDbContext<ShopDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services.AddScoped<IShopDbContext>(provider => provider.GetService<ShopDbContext>());
+
+            return services;
+        }
+    }
+}
