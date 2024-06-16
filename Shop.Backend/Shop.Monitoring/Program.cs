5936bb8dbd55f91c92767c3acd59d8c0e0770445
SmakleAst <SmakleLover@yandex.ru>
2024-06-16 19:32:57 +0300

    Set middlewares and launch url

--- Shop.Backend/Shop.Monitoring/Program.cs
+++ Shop.Backend/Shop.Monitoring/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllers();
+app.MapHealthChecksUI();
 
 app.Run();
