568f282531172036003f9fdbb852620165253101
SmakleAst <SmakleLover@yandex.ru>
2024-06-16 21:08:40 +0300

    Add identity persistence dependency injection

---
+++ Shop.Backend/Shop.Identity.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Application.Interfaces;
+
+namespace Shop.Identity.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddIdentityPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("ShopUsers");
+            services.AddDbContext<IdentityDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services.AddScoped<IIdentityDbContext>(provider => provider.GetService<IdentityDbContext>());
+
+            return services;
+        }
+    }
+}
